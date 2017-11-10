85d681ece9d9facaeb56c9df1a3f8b3e9260b904
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-11-10 23:50:13 +0000

    finish gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 'use strict';
 
-//include gulp, gulp-sass and browser-sync
+//include gulp, and all the plugins that are used
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync')
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
+    
 
 /* gulp.src() --> function that takes file globs and creates a stream of objects that represent the files
  pipe() --> allows stream to be piped through a function
@@ -25,7 +34,7 @@ gulp.task('browser-sync', function(){
         //specify all files that if modified the browser sync needs to reload
         './*.html',
         './css/*.css',
-        './img/.{png,gif,jpg}',
+        './img/*.{png,gif,jpg}',
         './js/*.js'
     ];
     //using the browserSync variable defined at the top
@@ -41,4 +50,51 @@ gulp.task('browser-sync', function(){
 gulp.task('default', ['browser-sync'], function(){
     //only starts after browser-sync is running
     gulp.start('sass:watch');
-})
+});
+
+//clean task
+gulp.task('clean', function(){
+    return del(['dist']);
+});
+
+//copy fonts
+gulp.task('copyfonts', function(){
+    //using globing patterns
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf, woff, eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+//image compression and minification
+gulp.task('imagemin', function(){
+    return gulp.src('img/*.{png,gif,jpg}')
+    .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
+    .pipe(gulp.dest('dist/img'));
+});
+
+//usemin task takes the html files and looks up the blocks, it concatenates, minifies, uglifies and generates 
+//the files for the dist folder
+gulp.task('usemin', function(){
+    return gulp.src('./*.html')
+    //flatmap takes multiple html files and starts parallel pipelines for each one of the files before making
+    //them converge
+    .pipe(flatmap(function(stream, file){
+        return stream
+        //pipe each stream through the usemin task
+        .pipe(usemin ({
+            css: [rev()], 
+            //as there are multiple html files, use a function
+            html: [function(){ return htmlmin({collapseWhitespace:true})}],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    //pipe resulting streams to dist folder
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean'], function(){
+    //clean should be first and then subsequent tasks can be completed in parallel
+     gulp.start('copyfonts', 'imagemin', 'usemin');
+
+});
